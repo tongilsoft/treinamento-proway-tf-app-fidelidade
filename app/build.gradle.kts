fecca26ddca82394d37dae5b8e08a1ff3b04aef9
Sergio Eduardo Fernandez Gonzalez <tongilsoft@gmail.com>
2026-08-13 23:20:14 -0300

    Problema com dependencia do material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
 
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.compose.material:material:1.5.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material:material-icons-extended:1.5.0")
