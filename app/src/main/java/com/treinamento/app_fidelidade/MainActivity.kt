5b7269d4daede426f18e0f0f26ce7a4eb945fc03
Alves <andre.c.alves@capgemini.com>
2026-08-16 19:56:24 -0300

    ajuste de merge  e estrutura de arquivos

--- app/src/main/java/com/treinamento/app_fidelidade/MainActivity.kt
+++ app/src/main/java/com/treinamento/app_fidelidade/MainActivity.kt
@@ -5,12 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.treinamento.app_fidelidade.ui.theme.FidelidadeTheme
 
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.treinamento.app_fidelidade.view.authentication.AuthenticationScreen
+import com.treinamento.app_fidelidade.view.fidelidade.FidelidadeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
